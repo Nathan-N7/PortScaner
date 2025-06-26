659eb1320ae30923153fd668af9635385a4717b5
Nathan-N7 <nathan.rod1710@gmail.com>
2025-06-26 16:41:51 -0300

    update print

--- PortScaner.rs
+++ PortScaner.rs
@@ -4,14 +4,21 @@ use std::time::Duration;
 
 fn scan_port(ip: &str)
 {
+    let mut res = 0;
     for port in 8080..=8081
     {
         let addr = format!("{}:{}", ip, port); //join
         if let Ok(_) = TcpStream::connect_timeout(&addr.parse().unwrap(), Duration::from_millis(100))
         {
-            println!("port {} is open", port);
+            println!(" -----  port {} is open", port);
+            res = 1;
         }
     }
+    if res == 0
+    {
+        println!(" -----  no port open!");
+    }
+    println!("");
     println!("end of execution");    
 }
 
@@ -25,5 +32,7 @@ fn main()
         .read_line(&mut ip)
         .expect("invalid read");
     let ip = ip.trim();
+    println!("");
+    println!("-------------------------");
     scan_port(ip);
 }
